b13ddab44379211d47141a17a920d45e9c879b37
Your Name <you@example.com>
2018-12-05 21:15:32 +0400

    files

--- index.js
+++ index.js
@@ -20,35 +20,29 @@
 // });
 
 
-// var express = require("express");
-// var app = express();
-
-// app.get("/",function(req, res){
-//     res.send("<h1>Hello world</h1>");
-// });
-
-// app.get("/:name/:surname", function(req, res){
-//     var name = req.params.name
-//     var surname = req.params.surname
-//     res.send("<h1>Hello " + name + " " + surname + " </h1>");
-
-// });
-// app.listen(3000, function(){
-//     console.log("Example is running on port 3000");
-// });
-
 var express = require("express");
 var app = express();
-app.get("/google", function(){
+
+app.get("/",function(req, res){
+    res.send("<h1>Hello world</h1>");
+});
+
+app.get("/:google", function(req, res){
+    var n = req.params.google;
+    res.redirect("http://google.com")
+});
+
+app.get("/search", function(req,res){
+    res.redirect("/")
+});
+
+app.get("/:name/:surname", function(req, res){
+    var name = req.params.name
+    var surname = req.params.surname
+    res.send("<h1>Hello " + name + " " + surname + " </h1>");
 
 });
-app.get("/name/:search", function(req, res){
-    var name = req.params.google;
-    var s = req.params.search
-    res.redirect("http://google.com" + "google.com/search?q=search value");
- });
- 
-
- app.listen(3000, function(){
+app.listen(3000, function(){
     console.log("Example is running on port 3000");
 });
+
