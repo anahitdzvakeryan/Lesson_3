3d92c9204b914dcbf0ab6613aabbb43a67cbf02c
user <you@example.com>
2018-12-07 16:03:04 +0400

    files

--- index.js
+++ index.js
@@ -20,35 +20,39 @@
 // });
 
 
+
+
+
 var express = require("express");
 var app = express();
 
-// app.get("/",function(req, res){
-//     res.send("<h1>Hello world</h1>");
-// });
-
-// app.get("/:google", function(req, res){
-//     var n = req.params.google;
-//     res.redirect("http://google.com")
-// });
-
-// app.get("/google/:search", function(req,res){
-//     var n = req.params.search;
-//     res.redirect("http://google.com/search?q=" + n);
-// });
-
-app.get("/*", function(req,res){
-    // var n = req.params.d;
-    res.redirect("404: Page not found", 404)
+app.get("/",function(req, res){
+    res.send("<h1>Hello world</h1>");
 });
 
-// app.get("/:name/:surname", function(req, res){
-//     var name = req.params.name
-//     var surname = req.params.surname
-//     res.send("<h1>Hello " + name + " " + surname + " </h1>");
+app.get("/google", function(req, res){
+    var n = req.params.google;
+    res.redirect("http://google.com")
+});
 
-// });
+app.get("/google/:search", function(req,res){
+    var n = req.params.search;
+    res.redirect("http://google.com/search?q=" + n);
+});
+
+app.get("/*", function(req,res){
+    res.redirect(404, "404: Page not found")
+});
+
+app.get("/:name/:surname", function(req, res){
+    var name = req.params.name
+    var surname = req.params.surname
+    res.send("<h1>Hello " + name + " " + surname + " </h1>");
+
+});
 app.listen(3000, function(){
     console.log("Example is running on port 3000");
 });
+
+
 
